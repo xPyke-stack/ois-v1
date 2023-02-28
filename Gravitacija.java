5cd989f13ee9ff2e4ebf92be6297347d0f2f3403
xPyke-stack <jakob.coper@gmail.com>
2023-02-28 21:10:03 +0100

    added read/write

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Gravitacija{
     public static void main(String[] args) {
         System.out.println("OIS!!!!!!!");
+        Scanner sc = new Scanner(System.in);
+        int height = sc.nextInt();
+
+        System.out.printf("%.2f", gravitationalAcceleration(height));
     }
 }
